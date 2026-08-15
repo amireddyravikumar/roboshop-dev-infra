04f744cc7ad05ff29a28f2ecf286e7011f97a009
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-08-15 23:04:25 +0530

    roboshop dev infra

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -15,6 +15,29 @@ resource "aws_instance" "mongodb" {
     local.common_tags
   )
 }
+
+resource "terraform_data" "mongodb" {
+  triggers_replace = [
+    aws_instance.mongodb.id 
+  ]
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password    = "DevOps321"
+    host        = aws_instance.mongodb.private_ip
+  }
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [ 
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}"
+     ]
+  }
+}
+
 resource "aws_instance" "redis" {
   ami = data.aws_ami.joindevops.id
   instance_type = "t3.micro"
@@ -32,6 +55,7 @@ resource "aws_instance" "redis" {
     local.common_tags
   )
 }
+
 resource "terraform_data" "redis" {
   triggers_replace = [
     aws_instance.redis.id 
