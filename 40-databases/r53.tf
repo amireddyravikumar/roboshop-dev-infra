f085ddc767909ec7c61c2f2f777848e0e2acd1fa
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-08-15 23:08:24 +0530

    roboshop dev infra

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -6,3 +6,11 @@ resource "aws_route53_record" "mongodb" {
   ttl     = 1
   records = [aws_instance.mongodb.private_ip]
 }
+# redis.amireddyravi.spae
+resource "aws_route53_record" "mongodb" {
+  zone_id = var.zone_id
+  name    = "redis-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.redis.private_ip]
+}
