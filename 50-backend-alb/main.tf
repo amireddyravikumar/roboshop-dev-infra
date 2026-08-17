dc7eb3df56d353f05c7c83e80c3cd394871173b3
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-08-17 23:44:29 +0530

    roboshop dev infra

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -18,8 +18,6 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.backend_alb.arn
   port              = "80"
   protocol          = "HTTP"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
 
   default_action {
     type = "fixed-response"
