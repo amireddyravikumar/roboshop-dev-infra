ee97ba895501c6944d8f5eb8cb5ca868886d3efa
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-08-17 19:25:11 +0530

    roboshop dev infra

---
+++ 50-backend-alb/main.tf
@@ -0,0 +1,46 @@
+resource "aws_lb" "backend_alb" {
+  name               = "${local.common_name}-backend-alb"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [local.backend_alb_sg_id]
+  subnets            = [local.private_subnet_ids]
+
+  enable_deletion_protection = true
+  tags = merge(
+    {
+        Name = "${local.common_name}-backend-alb"
+    },
+    local.common_tags
+  )
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.backend_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<h1>Hi, I am from HTTP Backend ALB created"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  name    = "*.backend-alb-${var.environment}.amireddyravi.space" # .amireddyravi.space
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+  allow_overwrite = true
+}
