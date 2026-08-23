9087c1bf84079ba5b6840d18027e9918784585a7
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-08-23 17:59:04 +0530

    roboshop dev infra

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -39,3 +39,15 @@ resource "aws_ec2_instance_state" "catalogue" {
   state       = "stopped"
   depends_on = [ data.aws_ami.joindevops ]
 }
+
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}" # roboshop-dev-catalogue-###
+  source_instance_id = aws_instance.catalogue.id
+  depends_on = [ aws_ec2_instance_state.catalogue ]
+  tags = merge(
+    {
+      Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+    },
+    local.common_tags
+  )
+}
