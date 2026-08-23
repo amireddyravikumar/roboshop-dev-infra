259eea8b74a00fecde384b35d83ab9660b18d0bf
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-08-23 12:30:10 +0530

    roboshop dev infra

--- 60-catalogue/bootstrap.sh
+++ 60-catalogue/bootstrap.sh
@@ -2,7 +2,7 @@
 
 component=$1 # catalogue
 environment=$2 # dev
-# app_version=$3
+app_version=$3
 dnf install ansible -y
 mkdir -p /var/log/roboshop/
 chown -R ec2-user:ec2-user /var/log/roboshop
@@ -13,4 +13,4 @@ cd /home/ec2-user
 git clone https://github.com/amireddyravikumar/roboshop-ansible-v3.git
 cd roboshop-ansible-v3
 git pull
-ansible-playbook -e component=$component -e env=$environment  roboshop.yaml
+ansible-playbook -e component=$component -e env=$environment -e app_version=$app_version roboshop.yaml
