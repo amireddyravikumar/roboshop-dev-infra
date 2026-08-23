5ef1fc34c65edbc538116a2acc2d7a2a18dafc17
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-08-23 17:47:42 +0530

    roboshop dev infra

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -33,3 +33,9 @@ resource "terraform_data" "catalogue" {
     ]
   }
 }
+
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped"
+  depends_on = [ data.aws_ami.joindevops ]
+}
