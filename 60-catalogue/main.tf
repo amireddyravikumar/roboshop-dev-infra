706dd4a98624070ff16199a33c330e3149381ff2
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-08-27 12:28:02 +0530

    roboshop dev infra

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -95,19 +95,66 @@ resource "aws_launch_template" "catalogue" {
   )
 }
 resource "aws_lb_target_group" "catalogue" {
-  name     = "${local.common_name}-catalogue"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = local.vpc_id
+  name                 = "${local.common_name}-catalogue"
+  port                 = 8080
+  protocol             = "HTTP"
+  vpc_id               = local.vpc_id
   deregistration_delay = 30
   health_check {
-    healthy_threshold = 2
-    interval = 10
-    matcher = "200-299"
-    path = "/health"
-    port = 8080
-    protocol = "HTTP"
-    timeout = 5
+    healthy_threshold   = 2
+    interval            = 10
+    matcher             = "200-299"
+    path                = "/health"
+    port                = 8080
+    protocol            = "HTTP"
+    timeout             = 5
     unhealthy_threshold = 2
+  }
+}
+resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${local.common_name}-catalogue"
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 120
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  force_delete              = false
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier = [local.private_subnet_id]
+
+  target_group_arns = [aws_lb_target_group.catalogue.arn]
+
+  dynamic "tag" {
+    for_each = merge(
+      {
+        Name = "${local.common_name}-catalogue"
+      },
+      local.common_tags
+    )
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+
+  }
+  # within specific period autoscaling should be successful to launch instance
+  timeouts {
+    delete = "15m"
+  }
+}
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
+  name                   = "${local.common_name}-catalogue"
+  policy_type            = "TargetTrackingScaling"
+  estimated_instance_warmup = 120
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 75.0
   }
 }
