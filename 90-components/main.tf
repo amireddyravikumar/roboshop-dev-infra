7b5356d7f48da3b267272e47bad177a1b5efd5cf
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-09-04 18:00:48 +0530

    roboshop dev infra

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,7 +1,8 @@
 module "components" {
-  for_each    = var.components
-  source = "git::https://github.com/amireddyravikumar/terraform-roboshop-component.git?ref=main"
-  environment = var.environment
-  component   = each.key
-  app_version = each.value.app_version
+  for_each      = var.components
+  source        = "git::https://github.com/amireddyravikumar/terraform-roboshop-component.git?ref=main"
+  environment   = var.environment
+  component     = each.key
+  app_version   = each.value.app_version
+  rule_priority = each.value.rule_priority
 }
